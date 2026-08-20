2355efb523ceb254a34d4f35f18cb9d625cf96bb
Joaquín Aqueveque <joaquin.aqueveque@ug.uchile.cl>
2026-08-19 20:35:27 -0400

    feat: avances lab 3 etapa 3 lista

--- labs/Lab3/src/pixellab/imagen.py
+++ labs/Lab3/src/pixellab/imagen.py
@@ -14,42 +14,95 @@ class Imagen:
 
     def __init__(self, img: np.ndarray) -> None:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __init__ antes de ejecutar el programa."
-        )
+        if not isinstance(img, np.ndarray):
+            raise TypeError(
+                "Debes entregar un arreglo de numpy como argumento del constructor de Imagen"
+            )
+        if img.ndim != 3:
+            raise ValueError("no se cumple 3 dimensiones")
+        if img.shape[-1] != 3:
+            raise ValueError("no se cumple 3 canales")
+        self.imagen = img
 
     def __add__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __add__ antes de ejecutar el programa."
-        )
+        if isinstance(other, Imagen):
+            if not other.imagen.shape == self.imagen.shape:
+                raise ValueError(
+                    "Las dimensiones de la imagen a operar (alto x ancho x canales) no calzan con las de la imagen original (alto x ancho x canales)"
+                )
+            other = other.imagen
+
+        resultado = (self.imagen + other).astype(int).copy()
+        resultado[resultado > 255] = 255
+        resultado[resultado < 0] = 0
+        return Imagen(resultado)
 
     def __radd__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
-        # Su código aquí
-        raise NotImplementedError(
-            "Completen __radd__ antes de ejecutar el programa."
-        )
+        if isinstance(other, Imagen):
+            if not other.imagen.shape == self.imagen.shape:
+                raise ValueError(
+                    "Las dimensiones de la imagen a operar (alto x ancho x canales) no calzan con las de la imagen original (alto x ancho x canales)"
+                )
+            other = other.imagen
+
+        resultado = (other + self.imagen).astype(int).copy()
+        resultado[resultado > 255] = 255
+        resultado[resultado < 0] = 0
+        return Imagen(resultado)
 
     def __sub__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __sub__ antes de ejecutar el programa."
-        )
+        if isinstance(other, Imagen):
+            if not other.imagen.shape == self.imagen.shape:
+                raise ValueError(
+                    "Las dimensiones de la imagen a operar (alto x ancho x canales) no calzan con las de la imagen original (alto x ancho x canales)"
+                )
+            other = other.imagen
+
+        resultado = (self.imagen - other).astype(int).copy()
+        resultado[resultado > 255] = 255
+        resultado[resultado < 0] = 0
+        return Imagen(resultado)
 
     def __rsub__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __rsub__ antes de ejecutar el programa."
-        )
+        if isinstance(other, Imagen):
+            if not other.imagen.shape == self.imagen.shape:
+                raise ValueError(
+                    "Las dimensiones de la imagen a operar (alto x ancho x canales) no calzan con las de la imagen original (alto x ancho x canales)"
+                )
+            other = other.imagen
+
+        resultado = (other - self.imagen).astype(int).copy()
+        resultado[resultado > 255] = 255
+        resultado[resultado < 0] = 0
+        return Imagen(resultado)
 
     def __mul__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __mul__ antes de ejecutar el programa."
-        )
+        if isinstance(other, Imagen):
+            if not other.imagen.shape == self.imagen.shape:
+                raise ValueError(
+                    "Las dimensiones de la imagen a operar (alto x ancho x canales) no calzan con las de la imagen original (alto x ancho x canales)"
+                )
+            other = other.imagen
+
+        resultado = (self.imagen * other).astype(int).copy()
+        resultado[resultado > 255] = 255
+        resultado[resultado < 0] = 0
+        return Imagen(resultado)
 
     def __rmul__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __rmul__ antes de ejecutar el programa."
-        )
+        if isinstance(other, Imagen):
+            if not other.imagen.shape == self.imagen.shape:
+                raise ValueError(
+                    "Las dimensiones de la imagen a operar (alto x ancho x canales) no calzan con las de la imagen original (alto x ancho x canales)"
+                )
+            other = other.imagen
+
+        resultado = (other * self.imagen).astype(int).copy()
+        resultado[resultado > 255] = 255
+        resultado[resultado < 0] = 0
+        return Imagen(resultado)
